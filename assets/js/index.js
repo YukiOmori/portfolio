83c67f60badfe9812fe2ee2be17f0f1be18f6176
Yuki Omori <yukiomori4211@gmail.com>
2018-05-19 18:05:21 +0900

    added responsive styles to work.html

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+let menuClickCounter = 0;
+$(() => {
+  $('#menu-button').on('click', function () {
+    $(this).data('click', ++menuClickCounter);
+    if (menuClickCounter % 2 === 1) {
+      console.log('クリックしたよ');
+      // $('#mini-nav-container').addClass('menu-clicked');
+      $('#menu-button').addClass('menu-clicked');
+      $('#mini-nav-container').fadeIn(300);
+      $('#mini-nav-wrapper a').fadeIn(1000);
+    } else {
+      // $('#mini-nav-container').removeClass('menu-clicked');
+      $('#menu-button').removeClass('menu-clicked');
+      $('#mini-nav-container').fadeOut(300);
+      $('#mini-nav-wrapper a').fadeOut(300);
+    }
+  });
+});
